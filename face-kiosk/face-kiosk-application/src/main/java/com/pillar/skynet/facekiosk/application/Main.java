aae912c4a8a268f7c24cf612d3c3620b7b9809df
Brandon Zeeb <phasebash@gmail.com>
2016-02-11 10:16:07 -0500

    Initial project structure.

---
+++ face-kiosk/face-kiosk-application/src/main/java/com/pillar/skynet/facekiosk/application/Main.java
@@ -0,0 +1,14 @@
+package com.pillar.skynet.facekiosk.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] s) {
+        LOG.info("Eye see you...");
+    }
+
+}
---
+++ face-kiosk/face-kiosk-application/src/test/java/com/pillar/skynet/facekiosk/application/MainTest.java
@@ -0,0 +1,12 @@
+package com.pillar.skynet.facekiosk.application;
+
+import org.junit.Test;
+
+public class MainTest {
+
+    @Test
+    public void shouldHavePublicStaticVoidMainMethod() {
+        Main.main(new String[] {});
+    }
+
+}
